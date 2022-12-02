e5151900055488ed7ca3ef3635fcef24fb1424a3
ignas-vaitkus <ignas.vaitkus7@gmail.com>
2022-12-02 15:09:36 +0200

    Added seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +16,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $roles[] = new Role();
+        $roles[] = new Role();
+        $roles[0]->name = 'admin';
+        $roles[1]->name = 'user';
+        $roles[0]->save();
+        $roles[1]->save();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $admin = \App\Models\User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'),
+            'role_id' => $roles[0]->id
+        ]);
+
+        $user = \App\Models\User::create([
+            'name' => 'test',
+            'email' => 'test@test.com',
+            'password' => Hash::make('test'),
+            'role_id' => $roles[1]->id
+        ]);
+
+        $school = \App\Models\School::create([
+            'code' => 123456789,
+            'name' => 'Saules gimnazija',
+            'address' => 'Savanoriu pr. XX',
+        ]);
+
+        \App\Models\Student::create([
+            'code' => 39912311111,
+            'first_name' => 'Vilius',
+            'last_name' => 'Vilesys',
+            'parent_id' => $user->id,
+            'school_id' => $school->code,
+            'grade' => 10
+        ]);
     }
 }
